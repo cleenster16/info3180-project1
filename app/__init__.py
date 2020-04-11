687ab2d6b5f5c4d49cd0d229d51aa141e382fcec
Cleon Foster <cleonfoster@Cleons-MacBook-Pro.local>
2020-04-10 19:40:16 -0500

    Changed heroku Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = x
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1owner:project1password123@localhost/project1"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1owner:project1password123@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://avjnmmvptqhfyl:1f1c55d464c63736ee5ae1583be480b6cf2d07ae8048598498a73fe0d856fb32@ec2-52-23-14-156.compute-1.amazonaws.com:5432/ddh5dio7si936j"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 app.config['UPLOAD_FOLDER'] = './app/static/images'
